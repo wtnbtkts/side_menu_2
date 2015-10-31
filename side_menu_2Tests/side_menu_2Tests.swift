d997485ce9fc2ab952c68e4382fce38af8133e54
Takatoshi Watanabe <abacus.taka1125.wtnbtkts@gmail.com>
2015-10-31 13:43:32 +0900

    Initial Commit

---
+++ side_menu_2Tests/side_menu_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  side_menu_2Tests.swift
+//  side_menu_2Tests
+//
+//  Created by Takatoshi Watanabe on 2015/10/31.
+//  Copyright © 2015年 Takatoshi Watanabe. All rights reserved.
+//
+
+import XCTest
+@testable import side_menu_2
+
+class side_menu_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
